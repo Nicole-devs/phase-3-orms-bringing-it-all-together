719fc5a838360470b47c770d23f5e8718c60c959
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-02 09:13:02 +0300

    .find_or_create_by-bonus1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
     attr_accessor :id, :name, :breed
   
-    
+
     def initialize(attributes)
       attributes.each {|key, value| self.send(("#{key}="), value)}
     end
@@ -93,7 +93,17 @@ class Dog
         self.new_from_db(row)
         end.first
     end
-      
+    
+
+    def self.find_or_create_by(name:, breed:)
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).first
+        if dog
+        new_from_db(dog)
+        else
+        create(name: name, breed: breed)
+        end
+    end
+    
 end
     
   
