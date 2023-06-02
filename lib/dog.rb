ebe1da253ffa9faea2bc41ee07d3e457527557b2
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-02 08:48:30 +0300

    Dog attributes

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,9 @@
 class Dog
-
-end
+    attr_accessor :id, :name, :breed
+  
+    def initialize(attributes)
+      attributes.each {|key, value| self.send(("#{key}="), value)}
+    end
+  
+  end
+  
